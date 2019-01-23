ac10f079dd68ab329fd5f986294e0db447120ba5
jumodada <864353360@qq.com>
2019-01-23 20:13:49 +0800

    添加icon，增加icon位置选择，iconPosition。 略微修改icon大小间距

--- src/button.vue
+++ src/button.vue
@@ -1,26 +1,63 @@
 <template>
-  <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href="`#icon-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+
+
 </template>
 <script>
-    export default {}
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 <style lang="scss">
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-      border-color: var(--border-color-hover);
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .content {
+            order: 2;
+        }
+        > .icon {
+            order: 1;
+            margin-left: 0;
+            margin-right: 0.3em;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left:0.3em;
+            }
+            > .content {
+                order: 1;
+            }
+        }
     }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-  }
 </style>
+
+
+
+
 
