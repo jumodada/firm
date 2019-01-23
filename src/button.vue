2e0d0264dfcea8ef768ff9a5d8903e64c993fa4b
jumodada <864353360@qq.com>
2019-01-23 20:56:53 +0800

    验证iconPosition的类型和值，为left或right

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,20 @@
 </template>
 <script>
     export default {
-        props: ['icon', 'iconPosition']
+        props: {
+            icon:{
+
+            },
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value) {
+                    return !(value !== 'left' && value !== 'right');
+                }
+
+            },
+
+        }
     }
 </script>
 <style lang="scss">
