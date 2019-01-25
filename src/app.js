0bc3556f7d8b6513e26c7d82e49e4ee2514a8413
jumodada <864353360@qq.com>
2019-01-25 22:10:02 +0800

    新建input，完成基础样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 
 new Vue({
     el: '#app',
---
+++ src/input.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="wrapper">
+    <input type="text"
+           :value="value"
+           :disabled="disabled"
+           :readonly="readonly"
+           :class="{error:error}"
+    >
+    <gIcon v-if="error" name="gantan" color="#F1453D"></gIcon>
+    <span v-if="error" :class="{errorMessage:error}">{{error}}</span>
+</div>
+
+</template>
+
+<script>
+    import gIcon from './icon'
+    export default {
+        name: "Input",
+        components:{
+            gIcon
+        },
+        props:{
+            value:{
+                type:String,
+
+            },
+            disabled:{
+                type:Boolean,
+                default:false
+            },
+            readonly:{
+                type:Boolean,
+                default:false
+            },
+            error:{
+                type:String
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+      $height:32px;
+      $border-color:#999;
+      $border-color-hover:#666;
+      $font-size:12px;
+      $box-shadow-color:rgba(0,0,0,0.5);
+      $red:#F1453D;
+
+
+
+      .wrapper{
+          font-size: $font-size;
+          display: inline-flex;
+          align-items: center;
+          > :not(:last-child){
+              margin-right: .3em;
+          }
+
+          > input{
+              height: $height;
+              border:1px solid $border-color;
+              padding: 0 8px;
+              font-size: inherit;
+
+              &:hover{
+                  border-color: $border-color-hover;
+              }
+              &:focus{
+                  box-shadow: inset 0 1px 3px $box-shadow-color;
+                  outline: none;
+              }
+              &[disabled]{
+                  border-color: #ccc;
+                  color:#ccc;
+                  cursor: not-allowed;
+              }
+              &.error{
+                  border-color: $red;
+
+              }
+
+          }
+          .errorMessage{
+              color: $red;
+          }
+      }
+
+
+
+</style>
