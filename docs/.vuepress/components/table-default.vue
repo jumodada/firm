3b26227f11edcb191f65b37b11e1e52ca5131991
jumodada <864353360@qq.com>
2019-04-12 23:56:54 +0800

    创建table测试用例

---
+++ docs/.vuepress/components/table-default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <x-table :columns="columns" :data="data">
+
+        </x-table>
+    </div>
+</template>
+
+<script>
+    import Table from '../../../src/data/table'
+    export default {
+        name: "table-default",
+        components:{
+            'x-table':Table
+        },
+        data(){
+            return {
+                currentPage:1,
+                columns:[],
+                data:[]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
