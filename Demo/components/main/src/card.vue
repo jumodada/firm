5235905100f306803567ef9da6259341ca3a8b4b
jumodada <864353360@qq.com>
2019-11-03 20:34:18 +0800

    add deep-clone method

--- Demo/components/main/src/card.vue
+++ Demo/components/main/src/card.vue
@@ -6,7 +6,10 @@
             一个基于Vue2.0的PC端组件库
         </div>
         <div class="card-group">
-            <div class="card-group-item">
+            <div class="card-group-item"
+                 @mouseenter="compass.play()"
+                 @mouseleave="compass.pause()"
+            >
                 <div class="card-group-item-title">
                     <svg ref="card" class="rotate" stroke="#fafaf1" stroke-width="24" viewBox="0 0 1024 1024"
                          width="200" height="240">
@@ -22,43 +25,92 @@
                     <div class="card-group-item-content-header">
                         指南
                     </div>
-                    <div class="card-group-item-content-main"></div>
-                    <div class="card-group-item-content-more"></div>
+                    <div class="card-group-item-content-main">
+                        包括组件功能、流程使用逻辑、以及友好的API设计
+                    </div>
+                    <div class="card-group-item-content-more">
+                        更多
+                        <x-icon color="white" name="right"></x-icon>
+                    </div>
                 </div>
             </div>
-            <div class="card-group-item"></div>
-            <div class="card-group-item"></div>
+            <div class="card-group-item">
+                <div class="card-group-item-title">
+                    <svg ref="components" class="stroke" stroke="#1ABC9C" stroke-width="20" viewBox="0 0 1024 1024"
+                         width="200" height="240">
+                        <path fill="#fafaf1"
+                              d="M894.3616 223.0272l-370.688-109.3632c-3.4816-1.024-7.168-1.6384-10.8544-1.6384-3.6864 0-7.168 0.4096-10.6496 1.4336l-351.232 102.4c-4.9152 0.2048-9.4208 1.6384-13.5168 3.8912l-6.5536 1.8432c-16.384 4.7104-27.648 19.6608-27.648 36.864L102.1952 733.184c0 15.9744 9.8304 30.3104 24.7808 36.0448L497.4592 909.312c4.3008 1.6384 9.0112 2.4576 13.5168 2.4576 4.5056 0 9.216-0.8192 13.5168-2.4576l371.5072-138.8544c14.9504-5.5296 24.9856-19.8656 24.9856-35.84l0.8192-474.5216c0-17.2032-11.0592-32.1536-27.4432-37.0688zM529.2032 177.5616l216.064 63.6928c7.168 2.048 7.168 12.288 0.2048 14.5408l-215.6544 68.4032c-11.6736 3.6864-23.9616 3.6864-35.6352 0l-214.8352-69.2224c-7.168-2.2528-6.9632-12.4928 0.2048-14.5408l216.8832-63.0784c10.6496-2.8672 22.1184-2.8672 32.768 0.2048zM185.344 287.5392l284.672 91.5456c7.168 2.2528 12.0832 9.0112 12.0832 16.5888v418.816c0 12.0832-12.0832 20.48-23.552 16.1792L173.2608 722.7392c-6.7584-2.6624-11.264-9.0112-11.264-16.384l0.8192-402.2272c-0.2048-11.8784 11.4688-20.0704 22.528-16.5888z"
+                              p-id="4388">
+                        </path>
+                    </svg>
+                </div>
+                <div class="card-group-item-content-more">
+                    更多
+                    <x-icon color="white" name="right"></x-icon>
+                </div>
+            </div>
+            <div class="card-group-item">
+                <div class="card-group-item-content-more">
+                    更多
+                    <x-icon color="white" name="right"></x-icon>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import anime from 'animejs/lib/anime.es'
+    import {deepClone} from "../../../../src/utils/common"
+
     export default {
         name: "card",
         data() {
             return {
-                isActive: false
+                isActive: false,
+                compass: {}
             }
         },
         mounted() {
-            anime({
-                targets: '.rotate',
-                duration: 1500,
-                rotate: {
-                    value: 360,
-                    duration: 1800,
+            this.compassInit()
+            this.componentsInit()
+        },
+        methods: {
+            compassInit() {
+                this.compass = anime({
+                    targets: '.rotate',
+                    duration: 1500,
+                    rotate: {
+                        value: 360,
+                        duration: 1800,
+                        easing: 'easeInOutSine'
+                    },
+                    delay: function (el, i) {
+                        return i * 250
+                    },
+                    direction: 'alternate',
+                    loop: true,
                     easing: 'easeInOutSine'
-                },
-                delay: function (el, i) {
-                    return i * 250
-                },
-                direction: 'alternate',
-                loop: true,
-                easing: 'easeInOutSine'
 
-            })
+                })
+                this.compass.pause()
+            },
+            componentsInit() {
+                let anime = deepClone(anime)
+                let path = anime.path('.stroke path')
+                this.compass = anime({
+                    targets: '.stroke',
+                    translateX: path('x'),
+                    translateY: path('y'),
+                    rotate: path('angle'),
+                    easing: 'linear',
+                    duration: 2000,
+                    loop: true
+                })
+                this.compass.pause()
+            }
         }
+
     }
 </script>
 
@@ -111,31 +163,45 @@
             &-item {
                 display: flex;
                 flex-direction: column;
+                position: relative;
                 align-items: center;
                 width: 300px;
                 height: 450px;
                 border-radius: 10px;
                 transition: .35s all ease;
                 cursor: pointer;
-                &:first-child{
+
+                &:first-child {
                     background-color: #27AE60;
-                    .card-group-item-title{
+
+                    .card-group-item-title {
                         background-color: #228743;
                     }
                 }
+
                 &:not(:first-child) {
                     margin-left: 3rem;
                 }
-                &:nth-child(2){
+
+                &:nth-child(2) {
                     background-color: #fac958;
-                    .card-group-item-title{
+
+                    .card-group-item-title {
                         background-color: #eebd58;
+                    }
+                }
+
+                &:nth-child(3) {
+                    background-color: #E74c3c;
+
+                    .card-group-item-title {
+                        background-color: #E74c3c;
                     }
                 }
 
                 &:hover {
                     box-shadow: 3px 0 4px 2px rgba(0, 0, 0, 0.15);
-                    transform: scale(1.01);
+                    transform: scale(1.02);
                 }
 
                 &-title {
@@ -146,8 +212,34 @@
                     align-items: center;
                     border-radius: 10px 10px 0 0;
                 }
-                &-content{
-                    
+
+                &-content {
+                    margin-top: 20px;
+                    padding: 20px;
+                    color: white;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+                    &-header {
+                        font-size: 24px;
+                    }
+
+                    &-main {
+                        padding: 0 10px;
+                        margin-top: 20px;
+                        font-size: 15px;
+                    }
+
+                    &-more {
+                        color: white;
+                        display: inline-flex;
+                        align-items: center;
+                        vertical-align: center;
+                        position: absolute;
+                        right: 30px;
+                        bottom: 30px;
+                    }
                 }
             }
         }
@@ -162,7 +254,7 @@
             background-image: linear-gradient(90deg, #ffd200 0%, #cc3133 100%);
         }
 
-        .rotate {
+        .rotate, .stroke {
             width: 100px;
         }
     }
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,7 +1,40 @@
 
 
-function deepCopy(data) {
-    //todo
+function deepClone(item,hash = new WeakMap()) {
+    if (!item) return item
+    if (hash.has(item))return hash.get(item);  //检查循环引用
+    var result,
+        type = Object.prototype.toString.call(item).slice(8,this.length-1),
+        fk= ['String','Number','Boolean','Function'].indexOf(type)>-1
+
+    if(fk){
+        result = item;//直接赋值
+    }else if(item.nodeType && typeof item.cloneNode === "function"){
+        result = item.cloneNode(true);          //是否是dom对象
+    }else{
+
+        let other = {           //需要递归或者其他的操作
+            Array() {
+                result = []
+                item.forEach((child, index)=>{
+                    hash.set(item, result);
+                    result[index] = deepClone(child,hash)
+                })
+            },
+            Date(){
+                result = new Date(item)
+            },
+            Object(){
+                result = {}
+                Reflect.ownKeys(item).forEach(child=>{
+                    hash.set(item, result);
+                    result[child] = deepClone(item[child],hash)
+                })
+            }
+        }
+        other[type]()
+    }
+    return result;
 }
 
 
@@ -11,4 +44,4 @@ function recurrenceOnceDeepCopy(obj) {
     return cloneObj
 }
 
-export {deepCopy,recurrenceOnceDeepCopy}
+export {deepClone,recurrenceOnceDeepCopy}
