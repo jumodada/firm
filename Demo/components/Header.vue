560d168f8ef7790a9ef2378bb8a06a8978d35d4b
jumodada <864353360@qq.com>
2019-11-11 15:49:14 +0800

    Header add github-icon

--- Demo/components/Header.vue
+++ Demo/components/Header.vue
@@ -10,6 +10,9 @@
             </router-link>
             <x-icon color="white" name="right"></x-icon>
         </div>
+        <div class="header-angle">
+            <x-icon color="white" font-size="35px" class="header-angle-icon" name="github1"></x-icon>
+        </div>
     </div>
 </template>
 
@@ -28,6 +31,7 @@
     @import "../assets/styles/color-select";
 
     .header {
+        position: relative;
         background-color: $brand1-1;
         z-index: 9999;
         width: 100%;
@@ -49,7 +53,7 @@
             align-items: center;
             justify-content: flex-end;
             text-align: end;
-            margin-right: 50px;
+            margin-right: 120px;
             &-item {
                 margin-left: 15px;
                 margin-right: 15px;
@@ -72,6 +76,27 @@
                         transform: translateY(2px);
                         opacity: 1;
                     }
+                }
+            }
+        }
+        &-angle{
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 120px;
+            height: 120px;
+            border-width: 60px;
+            border-style: solid;
+            border-color:#1d2022 #1d2022 #313639 #313639;
+            background-color: white;
+            &-icon{
+                cursor: pointer;
+                position: absolute;
+                top: -40px;
+                transform: rotate(45deg);
+                transition: .25s all ease;
+                &:hover{
+                    transform: rotate(56deg) scale(1.05);
                 }
             }
         }
