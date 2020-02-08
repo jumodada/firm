e836669e92aac3cf4f8b9509dbc4beb5ca8079d0
jumodada <864353360@qq.com>
2020-02-08 11:34:56 +0800

    add upload-list

---
+++ components/Upload/src/upload-list.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+        <li v-for="file in files">
+
+        </li>
+    </ul>
+</template>
+
+<script>
+    import Icon from '../../icon'
+    export default {
+        name: "upload-list",
+        components:{
+            Icon
+        },
+        props:{
+            files: {
+                type: Array,
+                default: () => []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
